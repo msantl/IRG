732b3ce699e4705f4eea4b73653892d8becbcfb0
Matija Santl <msantl.ck@gmail.com>
2013-03-19 11:07:39 +0100

    Finished with the third part

--- labos3/vjezba3.cpp
+++ labos3/vjezba3.cpp
@@ -1,18 +1,21 @@
 #include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <cmath>
 #include <vector>
 #include <GLUT/glut.h>
 using namespace std;
 
+#define EPS 1e-9
+
 // offset za kontrolnu liniju
-#define OFFSET 5
+#define OFFSET 4
 
 // cohen sutherland konstante
-#define YMAX 8
-#define YMIN 4
-#define XMAX 2
-#define XMIN 1
+#define MAXY 8
+#define MINY 4
+#define MAXX 2
+#define MINX 1
 
 void reshape(int width, int height);
 void display();
@@ -40,6 +43,12 @@ class segment_t {
   public:
     segment_t() : a(NULL), b(NULL){}
     segment_t(point_t *_a, point_t *_b) : a(_a), b(_b) {}
+
+    // copy constructor
+    segment_t(segment_t *s) {
+      a = new point_t(*(s->getStart()));
+      b = new point_t(*(s->getEnd()));
+    }
 
     ~segment_t() {
       delete a;
@@ -83,6 +92,31 @@ class state_t {
     int getWidth() {return width;}
     int getHeight() {return height;}
 
+    int getMaxX() {
+      if (odsijecanje) {
+        return 3 * width / 4;
+      }
+      return width;
+    }
+    int getMinX() {
+      if (odsijecanje){
+        return 1 * width / 4;
+      }
+      return 0;
+    }
+    int getMaxY() {
+      if (odsijecanje) {
+        return 3 * height / 4;
+      }
+      return height;
+    }
+    int getMinY() {
+      if (odsijecanje) {
+        return 1 * height / 4;
+      }
+      return 0;
+    }
+
     bool getKontrola() {return kontrola;}
     bool getOdsijecanje() {return odsijecanje;}
 
@@ -106,6 +140,11 @@ class state_t {
     }
 
 } STATE;
+
+
+void myBresenham(point_t *a, point_t *b);
+int getMask(double x, double y);
+bool myCohenSutherland(segment_t *s);
 
 int main(int argc, char **argv) {
   STATE.setWidth(500);
@@ -168,11 +207,20 @@ void renderScene() {
 
   glColor3f(0.0f, 0.0f, 0.0f);
   for (vector< segment_t* >::iterator it = segmenti.begin(); it != segmenti.end(); ++it) {
+    bool crtaj = true;
+    // kopiraj segment zbog odsijecanja
+    segment_t* seg = new segment_t(*it);
+    // nacrtaj vidljivi dio segmenta
     if (STATE.getOdsijecanje() == true) {
-      // izracunaj vidljivi dio segmenta
+      crtaj = myCohenSutherland(seg);
     }
 
-    // nacrtaj vidljivi dio segmenta
+    if (crtaj) {
+      myBresenham(seg->getStart(), seg->getEnd());
+    }
+
+    // obrisi segment
+    delete seg;
   }
 
   // tocni segmenti crveno
@@ -220,3 +268,167 @@ void mouseClick(int button, int status, int x, int y) {
     glutPostRedisplay();
   }
 }
+
+// kutevi od 0 do 90
+void bresenham_nacrtaj_cjelobrojni2(int xs, int ys, int xe, int ye) {
+  int x, yc, korekcija;
+  int a, yf;
+
+  if (ye - ys <= xe - xs) {
+    a = 2*(ye - ys);
+    yc = ys; yf = -(xe - xs); korekcija = -2*(xe - xs);
+    for (x = xs; x <= xe; ++x) {
+      glVertex2i(x, yc);
+      yf = yf + a;
+      if (yf >= 0) {
+        yf = yf + korekcija;
+        yc = yc + 1;
+      }
+    }
+  } else {
+    x = xe; xe = ye; ye = x;
+    x = xs; xs = ys; ys = x;
+    a = 2*(ye - ys);
+    yc = ys; yf = -(xe - xs); korekcija = -2*(xe - xs);
+    for (x = xs; x <= xe; ++x) {
+      glVertex2i(yc, x);
+      yf = yf + a;
+      if (yf >= 0) {
+        yf = yf + korekcija;
+        yc = yc + 1;
+      }
+    }
+  }
+}
+
+
+// kutevi od 0 do -90
+void bresenham_nacrtaj_cjelobrojni3(int xs, int ys, int xe, int ye) {
+  int x, yc, korekcija;
+  int a, yf;
+  if (-(ye-ys) <= xe - xs) {
+    a = 2*(ye - ys);
+    yc = ys;  yf = (xe - xs); korekcija = 2*(xe - xs);
+    for (x = xs; x <= xe; ++x) {
+      glVertex2i(x, yc);
+      yf = yf + a;
+      if (yf <= 0) {
+        yf = yf + korekcija;
+        yc = yc - 1;
+      }
+    }
+  } else {
+    x = xe; xe = ys; ys = x;
+    x = xs; xs = ye; ye = x;
+    a = 2*(ye - ys);
+    yc = ys; yf = (xe - xs); korekcija = 2*(xe - xs);
+    for (x = xs; x <= xe; ++x) {
+      glVertex2i(yc, x);
+      yf = yf + a;
+      if (yf <= 0) {
+        yf = yf + korekcija;
+        yc = yc - 1;
+      }
+    }
+  }
+}
+
+void bresenham_nacrtaj_cjelobrojni(int xs, int ys, int xe, int ye) {
+  if (xs <= xe) {
+    if (ys <= ye) {
+      bresenham_nacrtaj_cjelobrojni2(xs, ys, xe, ye);
+    } else {
+      bresenham_nacrtaj_cjelobrojni3(xs, ys, xe, ye);
+    }
+  } else {
+    if (ys >= ye) {
+      bresenham_nacrtaj_cjelobrojni2(xe, ye, xs, ys);
+    } else {
+      bresenham_nacrtaj_cjelobrojni3(xe, ye, xs, ys);
+    }
+  }
+}
+
+void myBresenham(point_t *a, point_t *b) {
+  glBegin(GL_POINTS);
+  bresenham_nacrtaj_cjelobrojni(a->getX(), a->getY(), b->getX(), b->getY());
+  glEnd();
+  return;
+}
+
+bool myCohenSutherland(segment_t *s) {
+  bool ret = false;
+
+  double x1 = s->getStart()->getX();
+  double y1 = s->getStart()->getY();
+
+  double x2 = s->getEnd()->getX();
+  double y2 = s->getEnd()->getY();
+
+  while (1) {
+    int maskStart = getMask(x1, y1);
+    int maskEnd = getMask(x2, y2);
+
+    if (maskStart == 0 && maskEnd == 0) {
+      // potpuno vidljiva
+      ret = true;
+      break;
+    } else if ((maskStart & maskEnd) != 0) {
+      // nije vidljivo
+      ret = false;
+      break;
+    } else {
+      // odsijecanje
+
+      int mask = maskStart ? maskStart : maskEnd;
+      double x, y;
+
+      // y - y1 = (y2 - y1) * (x - x1) / (x2 - x1)
+
+      if (mask & MAXY) {
+        x = x1 + (x2 - x1) * (STATE.getMaxY() - y1) / (y2 - y1);
+        y = STATE.getMaxY();
+      } else if (mask & MINY) {
+        x = x1 + (x2 - x1) * (STATE.getMinY() - y1) / (y2 - y1);
+        y = STATE.getMinY();
+      } else if (mask & MAXX) {
+        y = y1 + (y2 - y1) * (STATE.getMaxX() - x1) / (x2 - x1);
+        x = STATE.getMaxX();
+      } else if (mask & MINX) {
+        y = y1 + (y2 - y1) * (STATE.getMinX() - x1) / (x2 - x1);
+        x = STATE.getMinX();
+      }
+
+      // update the coordinates
+      if (mask == maskStart) {
+        x1 = x;
+        y1 = y;
+      } else {
+        x2 = x;
+        y2 = y;
+      }
+
+    }
+  }
+
+  // update the coordinates
+  s->getStart()->setX((int)x1);
+  s->getStart()->setY((int)y1);
+  s->getEnd()->setX((int)x2);
+  s->getEnd()->setY((int)y2);
+
+  return ret;
+}
+
+int getMask(double x, double y) {
+  int mask = 0;
+
+  if (y > STATE.getMaxY()) mask |= MAXY;
+  if (y < STATE.getMinY()) mask |= MINY;
+  if (x > STATE.getMaxX()) mask |= MAXX;
+  if (x < STATE.getMinX()) mask |= MINX;
+
+  return mask;
+}
+
+
